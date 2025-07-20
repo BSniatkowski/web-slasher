024d3626e668416c8c5dc541ced5c04c533eb741
Bartłomiej Śniatkowski <bartlomiej.sniatkowski.mac@gmail.com>
2025-07-20 19:46:19 +0200

    added three with types

---
+++ src/game/game.ts
@@ -0,0 +1,39 @@
+import {
+  Scene,
+  PerspectiveCamera,
+  WebGLRenderer,
+  BoxGeometry,
+  MeshBasicMaterial,
+  Mesh,
+} from "three";
+
+export const createGameInstance = () => {
+  const scene = new Scene();
+  const camera = new PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight
+  );
+  const renderer = new WebGLRenderer();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  document.body.appendChild(renderer.domElement);
+
+  const geometry = new BoxGeometry(1, 1, 1);
+  const material = new MeshBasicMaterial({ color: 0x00ff00 });
+
+  const cube = new Mesh(geometry, material);
+
+  scene.add(cube);
+
+  camera.position.z = 5;
+
+  const animate = () => {
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    renderer.render(scene, camera);
+  };
+
+  renderer.setAnimationLoop(animate);
+};
